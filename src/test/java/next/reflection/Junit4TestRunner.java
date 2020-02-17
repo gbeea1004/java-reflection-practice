a812a7cb8c4a9f272c15fd4b3d6387ae81ae6e2f
geonHee <corqkrtk3@naver.com>
2020-02-17 14:38:14 +0900

    Test Reflection Execute @Test annotation method

--- src/test/java/next/reflection/Junit4TestRunner.java
+++ src/test/java/next/reflection/Junit4TestRunner.java
@@ -2,10 +2,26 @@ package next.reflection;
 
 import org.junit.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+        Junit4Test instance = clazz.newInstance();
+        Arrays.stream(declaredMethods)
+              .filter(m -> m.isAnnotationPresent(MyTest.class))
+              .forEach(m -> {
+                  try {
+                      m.invoke(instance);
+                  } catch (IllegalAccessException e) {
+                      e.printStackTrace();
+                  } catch (InvocationTargetException e) {
+                      e.printStackTrace();
+                  }
+              });
     }
 }
